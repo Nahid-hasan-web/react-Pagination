a87a9060ebf3185dd80ca12861af9d16ba1c945e
Nahid-hasan-web <nahidhasanniyan@gmail.com>
2025-07-18 16:34:42 +0600

    previous and next button added

--- src/Pagination.jsx
+++ src/Pagination.jsx
@@ -1,21 +1,68 @@
+import React from "react";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+
 const Pagination = ({ totalPages, currentPage, setPage }) => {
+  const pageNumbers = Array.from({ length: totalPages }, (_, i) => i + 1);
+
+  const handlePrev = () => {
+    if (currentPage > 1) setPage(currentPage - 1);
+  };
+
+  const handleNext = () => {
+    if (currentPage < totalPages) setPage(currentPage + 1);
+  };
+
   return (
     <div style={{ marginTop: 20, textAlign: "center" }}>
-      {Array.from({ length: totalPages }, (_, i) => (
+      {/* Prev */}
+      <button
+        onClick={handlePrev}
+        disabled={currentPage === 1}
+        style={{
+          margin: "0 5px",
+          padding: "6px 10px",
+          border: "1px solid #ccc",
+          background: "#eee",
+          cursor: "pointer",
+          opacity: currentPage === 1 ? 0.5 : 1,
+        }}
+      >
+        <FaArrowLeft />
+      </button>
+
+      {/* Page numbers */}
+      {pageNumbers.map((num) => (
         <button
-          key={i}
-          onClick={() => setPage(i + 1)}
+          key={num}
+          onClick={() => setPage(num)}
           style={{
-            margin: "0 5px",
-            padding: "5px 10px",
-            background: currentPage === i + 1 ? "#FFA62F" : "#eee",
+            margin: "0 3px",
+            padding: "6px 12px",
+            background: currentPage === num ? "#FFA62F" : "#fff",
+            color: currentPage === num ? "#fff" : "#000",
             border: "1px solid #ccc",
             cursor: "pointer",
           }}
         >
-          {i + 1}
+          {num}
         </button>
       ))}
+
+      {/* Next */}
+      <button
+        onClick={handleNext}
+        disabled={currentPage === totalPages}
+        style={{
+          margin: "0 5px",
+          padding: "6px 10px",
+          border: "1px solid #ccc",
+          background: "#eee",
+          cursor: "pointer",
+          opacity: currentPage === totalPages ? 0.5 : 1,
+        }}
+      >
+        <FaArrowRight />
+      </button>
     </div>
   );
 };
